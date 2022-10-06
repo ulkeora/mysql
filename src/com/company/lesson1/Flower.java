3836bd0edb94680d96fed80e5f4e4ed25280b954
Aktan <aktan17720017@gmail.com>
2022-10-06 19:04:26 +0600

    Initial commit

---
+++ src/com/company/lesson1/Flower.java
@@ -0,0 +1,10 @@
+package com.company.lesson1;
+
+public class Flower {
+    public static void main(String[] args) {
+        for(int i=10;i<=29;i++){
+            System.out.println(i);
+
+        }
+    }
+}
